299bff3252d61e2fae511dfa9a9f893e306dbaf8
Israel Façanha <israel.facanha@gmail.com>
2023-08-27 23:49:53 -0300

    Upgrade Icon component to be more useful
    Icon component can be used as button and link now

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,17 +1,75 @@
 import './Icon.module.scss'
+import React, { ButtonHTMLAttributes, MouseEventHandler, useMemo } from 'react'
 
-type IconName = 'search' | 'close'
+type IconType = 'presentational' | 'button' | 'link'
+type IconName = 'search' | 'close' | 'chevron_left'
 
-interface IconProps {
+interface IconPropsBase<IT extends IconType> {
+    type: IT
     icon: IconName
     size?: 'inherit'
+    className?: string
 }
 
-const Icon: React.FC<IconProps> = ({ icon, size }) => {
+type IconPresentationalProps = IconPropsBase<'presentational'>
+
+interface IconClickableProps {
+    ariaLabel?: string
+    ariaLabelledBy?: string
+}
+
+interface IconButtonProps extends IconPropsBase<'button'>, IconClickableProps {
+    onClick: MouseEventHandler<HTMLButtonElement>
+    buttonType?: ButtonHTMLAttributes<HTMLButtonElement>['type']
+}
+
+interface IconLinkProps extends IconPropsBase<'link'>, IconClickableProps {
+    href: string
+}
+
+export type IconProps = IconPresentationalProps | IconButtonProps | IconLinkProps
+
+function getTagProps<T extends keyof JSX.IntrinsicElements>(
+    tag: T,
+    attr?: JSX.IntrinsicElements[T]
+): [T, object] | [T, JSX.IntrinsicElements[T]] {
+    if (!attr) {
+        return [tag, {}]
+    }
+
+    return [tag, attr]
+}
+
+const Icon: React.FC<IconProps> = ({ icon, size, className = '', ...props }) => {
+    const [Tag, tagProps] = useMemo(() => {
+        switch (props.type) {
+            case 'button': {
+                const { onClick, ariaLabel, ariaLabelledBy, buttonType: type } = props
+
+                return getTagProps('button', {
+                    'aria-label': ariaLabel,
+                    'aria-labelledby': ariaLabelledBy,
+                    onClick,
+                    type,
+                })
+            }
+
+            case 'link': {
+                const { href, ariaLabel, ariaLabelledBy } = props
+
+                return getTagProps('a', { 'aria-label': ariaLabel, 'aria-labelledby': ariaLabelledBy, href })
+            }
+
+            default:
+                return getTagProps('span')
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.type])
+
     return (
-        <span style={{ fontSize: size }} className="material-symbols-outlined">
+        <Tag className={`material-symbols-outlined ${className}`} style={{ fontSize: size }} {...tagProps}>
             {icon}
-        </span>
+        </Tag>
     )
 }
 
