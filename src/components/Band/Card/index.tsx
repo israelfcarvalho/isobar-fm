cbaac74452efe8cb94700d0090e403a98d91a0d0
Israel Façanha <israel.facanha@gmail.com>
2023-08-28 21:31:30 -0300

    Update component BandCard styles

--- src/components/Band/Card/index.tsx
+++ src/components/Band/Card/index.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom'
 import { Band } from '../../../types/entities/band'
+import styles from './BandCard.module.scss'
 
 interface BandCardProps {
     band: Band
@@ -6,11 +8,11 @@ interface BandCardProps {
 
 const BandCard: React.FC<BandCardProps> = ({ band }) => {
     return (
-        <section>
-            <img src={band.image} alt={band.name} />
-            <h1>{band.name}</h1>
-            <p>{band.numPlays} Plays</p>
-        </section>
+        <Link to={band.id} className={styles.card}>
+            <img className={styles.img} src={band.image} alt={band.name} />
+            <span className={styles.name}>{band.name}</span>
+            <span className={styles.plays}>{band.numPlays} Plays</span>
+        </Link>
     )
 }
 
--- src/utils/hooks/classname.ts
+++ src/utils/hooks/classname.ts
@@ -1,12 +1,8 @@
 export function className(...classeNames: Array<string | false>): string {
     const initialClassName = ''
 
-    console.log({ classeNames })
-
     return classeNames.reduce<string>((fullClassName, className) => {
         const hasClassName = !!className && !!className.trim()
-
-        console.log({ hasClassName, className })
 
         if (!hasClassName) {
             return fullClassName
